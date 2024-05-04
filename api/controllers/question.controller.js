7eadc599e3ff70b215c884227975d1f8e3dd1ede
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-05-04 14:29:51 +0300

    add edit and delete functionalities

--- api/controllers/question.controller.js
+++ api/controllers/question.controller.js
@@ -2,7 +2,7 @@ import Question from "../models/question.model.js";
 
 export const getQuestions = async (req, res) => {
   try {
-    const { type } = req.body;
+    const type = req.params.type;
     const questions = await Question.find({ type });
     res.status(201).json({ questions });
   } catch (error) {
@@ -17,6 +17,30 @@ export const addQuestion = async (req, res) => {
     const newQuestion = new Question(req.body);
     await newQuestion.save();
     res.status(201).json({ message: "Question added successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", errors: error.message });
+  }
+};
+
+export const editQuestion = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Question.findByIdAndUpdate(id, req.body);
+    res.status(201).json({ message: "Your question was edit" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", errors: error.message });
+  }
+};
+
+export const deleteQuestion = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Question.findByIdAndDelete(id);
+    res.status(201).json({ message: "Your question was delete successfully" });
   } catch (error) {
     res
       .status(500)
--- api/routers/question.router.js
+++ api/routers/question.router.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addQuestion, getQuestions } from "../controllers/question.controller.js";
+import { addQuestion, deleteQuestion, editQuestion, getQuestions } from "../controllers/question.controller.js";
 
 const router = express.Router();
 
-router.get("/getQuestions", getQuestions);
+router.get("/getQuestions/:type", getQuestions);
 router.post("/addQuestion", addQuestion);
+router.put("/editQuestion/:id", editQuestion);
+router.delete("/deleteQuestion/:id", deleteQuestion);
 
 export default router;
