a2302fe43d33e8b5f55a24a3d614b8ab2d49d362
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-05-04 12:44:24 +0300

    modify the type of an endpoint

--- api/routers/question.router.js
+++ api/routers/question.router.js
@@ -3,7 +3,7 @@ import { addQuestion, getQuestions } from "../controllers/question.controller.js
 
 const router = express.Router();
 
-router.post("/getQuestions", getQuestions);
+router.get("/getQuestions", getQuestions);
 router.post("/addQuestion", addQuestion);
 
 export default router;
