ff6a51ea61081c7d6f71f65b22b9de770dd2ffe7
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-05-05 08:15:18 +0300

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="home">
+      <h1>Welcome to: Ce spun romanii</h1>
+      <Link to="/dashboard">
+        <button>Go to dashboard</button>
+      </Link>
+      <p>Created by Parfene Daniel Emanuel</p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
