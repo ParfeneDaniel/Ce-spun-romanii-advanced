69fc0c81f36544a0230e745863de878a50a81615
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-05-10 21:22:24 +0300

    reconfig project

--- client/src/lib/apiRequest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const apiRequest = axios.create({
-  baseURL: "http://localhost:8080/api/question",
-  withCredentials: false,
-});
-
-export default apiRequest;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routers/router";
-import { QuestionContextProvider } from "./contexts/QuestionContext";
-import { AddQuestionContextProvider } from "./contexts/AddQuestionContext";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QuestionContextProvider>
-      <AddQuestionContextProvider>
-        <RouterProvider router={router} />
-      </AddQuestionContextProvider>
-    </QuestionContextProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
