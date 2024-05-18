4c1d6b705b3bea9c6506a8035261bf58e2d14ea7
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-05-18 17:35:51 +0300

    add home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,21 +7,39 @@ const Home = () => {
   };
   return (
     <div className="backgroundHome">
-      <h1>Ce spun romanii?</h1>
-      <p>Coder:</p>
-      <p>Parfene Daniel</p>
-      <p id="designer">Designer:</p>
-      <p>Grozav Medeea</p>
-      <p id="questionsMakers">Questions makers:</p>
-      <p>Ardelean Raul</p>
-      <p>Bulzan Ionut</p>
-      <p>Schipor Devis</p>
-      <p>Todea Alexandra</p>
-      <p id="host">Host:</p>
-      <p>Moț Raul</p>
-      <button onClick={handleStartGameClick} id="startGame">
-        Start game
-      </button>
+      <div className="title">
+        <h1>Ce spun romanii?</h1>
+      </div>
+      <div className="content">
+        <div className="creators">
+          <div className="coder">
+            <h3>Coder:</h3>
+            <p>Parfene Daniel</p>
+          </div>
+          <div className="designer">
+            <h3>Designer:</h3>
+            <p>Grozav Medeea</p>
+          </div>
+        </div>
+        <div className="button">
+          <button onClick={handleStartGameClick}>
+            Start game
+          </button>
+        </div>
+        <div className="helpers">
+          <div className="questionsMakers">
+            <h3>Helpers:</h3>
+            <p>Ardelean Raul</p>
+            <p>Bulzan Ionut</p>
+            <p>Schipor Devis</p>
+            <p>Todea Alexandra</p>
+          </div>
+          <div className="host">
+            <h3>Host:</h3>
+            <p>Moț Raul</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
